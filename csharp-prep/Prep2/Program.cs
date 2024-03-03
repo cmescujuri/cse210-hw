cbb115062aef1c3496832f4c0b8b2c615470bd7b
cmescujuri <esc12005@byui.edu>
2024-03-02 20:12:31 -0700

    Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradePercentString = Console.ReadLine();
+        int gradePercent = int.Parse(gradePercentString);
+
+        string grade = "";
+
+        if (gradePercent >= 90)
+        {
+            grade = "A";
+        }
+
+        else if (gradePercent >= 80)
+        {
+            grade = "B";
+        }
+        
+        else if (gradePercent >= 70)
+        {
+            grade = "C";
+        }
+        
+        else if (gradePercent >= 60)
+        {
+            grade = "D";
+        }
+
+        else 
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade is {grade}.");
+
+        if (gradePercent >= 70)
+        {
+            Console.WriteLine("Hooray -- you passed!");
+        }
+
+        else 
+        {
+            Console.WriteLine("You failed. Better luck next time!");
+        }
     }
 }
